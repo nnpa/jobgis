bd92fe71d488ebafea3e83f4c41202fe681d069c
Ваше Имя <you@example.com>
2022-10-10 23:14:35 +0500

    first commit

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -20,6 +20,7 @@ class AppController extends Controller{
             ];
             
             if($user->name == "" OR $user->surname == "" OR $user->phone == "" OR $user->city){
+                echo $user->name . "" .$user->surname . " " . $user->phone . " " .$user->city;exit;
                 if(!in_array($action->actionMethod, $actions)){
                     return $this->redirect("/site/addinfo"); 
                 }
