8fee1ba916346fc4854231ce400aae3d2ef4a38f
Ваше Имя <you@example.com>
2022-10-10 20:37:54 +0500

    first commit

--- views/site/candidate.php
+++ views/site/candidate.php
@@ -1,2 +1,2 @@
-<h3>Разместите резюме</h3>
-<a href="/site/registercandidate" class="btn btn-success">Разместить резюме</a>
+<h3>Создать резюме на jobgis.ru</h3>
+<a href="/site/registercandidate" class="btn btn-success">Создать резюме</a>
