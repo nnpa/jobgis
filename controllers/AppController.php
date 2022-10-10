e18d514520235a1edbf7054e2d006b6610e56bea
Ваше Имя <you@example.com>
2022-10-10 22:45:45 +0500

    first commit

--- controllers/AppController.php
+++ controllers/AppController.php
@@ -11,22 +11,31 @@ class AppController extends Controller{
         if(!Yii::$app->user->isGuest){
             $user = Yii::$app->user->identity;
             $user = Users::find()->where(["id" => $user->id])->one();
+            $actions = [
+                "actionAddinfo",
+                "actionLogin",
+                "actionLogin",
+                "actionAddinn",
+                "actionVerify"
+            ];
+            
             if($user->name == "" OR $user->surname == "" OR $user->phone == "" OR $user->city == ""){
-                if($action->actionMethod != "actionAddinfo" AND $action->actionMethod != "actionLogin"  AND $action->actionMethod != "actionLogin" AND $action->actionMethod != "actionAddinn" AND $action->actionMethod != "actionVerify"){
+                if(!in_array($action->actionMethod, $actions)){
                     return $this->redirect("/site/addinfo"); 
                 }
             }
             
             if($user->firm_id != 0){
                 if($user->firm->inn == 0 OR $user->firm->category == "" AND $user->firm->city == "" AND $action->actionMethod != "actionVerify"){
-                    if($action->actionMethod != "actionAddinn" AND $action->actionMethod != "city" AND $action->actionMethod != "actionLogin"){
+                    if(!in_array($action->actionMethod, $actions)){
                         return $this->redirect("/site/addinn"); 
                     }
                 }
             }
+            
             if($user->firm_id != 0){
                 if($user->firm->verify == 0){
-                    if($action->actionMethod != "actionAddinfo" AND $action->actionMethod != "actionLogin"  AND $action->actionMethod != "actionLogin" AND $action->actionMethod != "actionAddinn" AND $action->actionMethod != "actionVerify"){
+                    if(!in_array($action->actionMethod, $actions)){
                         return $this->redirect("/site/verify"); 
                     }
                 }
