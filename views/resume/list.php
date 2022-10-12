ec6d392e2e2ce19c213d314e8fbb628e97a0620d
Ваше Имя <you@example.com>
2022-10-12 05:35:39 +0500

    first commit

--- views/resume/list.php
+++ views/resume/list.php
@@ -1,5 +1,5 @@
 <h3>Мои резюме</h3>
-<a href="/resume/new" class="btn btn-success">Новое резюме</a><br>
+<a href="/resume/new" onClick="return confirm('Создать резюме?')" class="btn btn-success">Новое резюме</a><br>
 <?php foreach($resumes as $resume):?>
     <a href="/resume/edit?id=<?php echo $resume->id;?>"><?php echo $resume->vacancy;?></a>
     <small><a target="_blank" href="/resume/show?id=<?php echo $resume->id;?>">Ссылка</a></small>
