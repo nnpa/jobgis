628e5cf053f2e51242fb1c5fb7e2ba7f6d914d56
Ваше Имя <you@example.com>
2022-10-13 19:16:47 +0500

    first commit

--- modules/admin/views/default/managers.php
+++ modules/admin/views/default/managers.php
@@ -1,6 +1,27 @@
+<style>
+    td {
+        border:1px solid black;
+        padding:5px;
+    }
+</style>
 <h3>Менеджеры</h3>
+<table>
+    <tr>
+        <td>Фамилия</td>
+        <td>Имя</td>
+        <td>Отчество</td>
+        <td>email</td>
+
+    </tr>
 <?php foreach($managers as $manager):?>
+    <tr>
     <?php if(is_object($manager->user)):?>
-        <?php echo $manager->user->name . " " . $manager->user->surname;?>
+        <td><?php echo $manager->user->surname?></td>
+        <td><?php echo $manager->user->name?></td>
+        <td><?php echo $manager->user->patronymic?></td>
+        <td><?php echo $manager->user->email?></td>
+
     <?php endif;?>
+    </tr>
 <?php endforeach; ?>
+</table>
