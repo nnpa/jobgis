099839338c2b8d1da77709e0e967815af72e1bf2
Ваше Имя <you@example.com>
2022-10-15 01:00:23 +0500

    first commit

--- views/vacancy/show.php
+++ views/vacancy/show.php
@@ -26,7 +26,7 @@
         до <?php echo $vacancy->costto;?>
    <?php endif;?>
  
- <?php if($vacancy->costto != 0 AND $vacancy->costfrom != 0):?>
+ <?php if($vacancy->costto != 0 OR $vacancy->costfrom != 0):?>
     <?php echo $vacancy->cash;?> <?php echo $vacancy->cashtype;?> <br>
  <?php endif;?>
 </span>
