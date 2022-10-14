1e8d846a6fb5c8caa7c2c7b6be66759844722747
Ваше Имя <you@example.com>
2022-10-14 21:27:10 +0500

    first commit

--- views/vacancy/show.php
+++ views/vacancy/show.php
@@ -18,13 +18,12 @@
 <div style="padding-left: 50px;">
 <h3><?php echo $vacancy->name;?></h3>
 <span class="vacancy_compensation">
-    <?php var_dump((bool)$vacancy->costto);exit;?>
     <?php if((bool)$vacancy->costto):?>
 
         от <?php echo $vacancy->costfrom;?> 
    <?php endif;?>
 
-   <?php if($vacancy->costto != 0):?>
+   <?php if((bool)$vacancy->costto):?>
         до <?php echo $vacancy->costto;?>
    <?php endif;?>
         
