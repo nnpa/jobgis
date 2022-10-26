6de921093ce928226a5df3f64763096bfad8db22
Ваше Имя <you@example.com>
2022-10-26 09:24:51 +0500

    first commit

--- views/site/index.php
+++ views/site/index.php
@@ -73,7 +73,7 @@
 
         <div>&nbsp;</div>
 <br>
-<div style="position:absolute;bottom:0;">
+<div style="position:absolute;bottom:20px;">
 <h5>Информационные партнеры</h5>
 
 <?php foreach($partners as $partner):?> 
