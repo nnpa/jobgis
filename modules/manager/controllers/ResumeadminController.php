aad415c6dc58d8d7b4d2f2c675f60f001990b2ea
Ваше Имя <you@example.com>
2022-11-07 14:56:17 +0500

    first commit

--- modules/manager/controllers/ResumeadminController.php
+++ modules/manager/controllers/ResumeadminController.php
@@ -42,16 +42,134 @@ class ResumeadminController extends Controller
      */
     public function actionIndex()
     {
-        $searchModel = new ResumeSearch();
-        $dataProvider = $searchModel->search($this->request->queryParams);
-        $dataProvider->sort = [
-            'defaultOrder' => [
-                'id' => SORT_DESC,
-            ]
-        ];
-        return $this->render('index', [
-            'searchModel' => $searchModel,
-            'dataProvider' => $dataProvider,
+  if(isset($_GET["page"]) && !empty($_GET["page"])){
+        $page = $_GET["page"];
+        }else {
+            $page = 1;
+        }
+        
+        $perPage = 20;
+        
+        $conn = mysqli_connect("localhost","root","g02091988","jobgis");
+        
+        if(!Yii::$app->user->isGuest){
+            $user = Yii::$app->user->identity;
+            $city = $user->city;
+        }else{
+            $city = "";
+        }
+        $sql = "SELECT * FROM `resume` WHERE 1=1  AND `vacancy` != 'Заполните должность' AND `verify` = 1";
+        $url = "/admin/resumeadmin/index?test=1";
+        $sqlCount = "SELECT COUNT(*) FROM `resume` WHERE 1=1 != 'Заполните должность' AND `verify` = 1";
+        
+        if(isset($_GET["name"]) && !empty($_GET['name'])){
+            $name = $_GET["name"];
+            $sql .= " AND `vacancy` = '". mysqli_real_escape_string($conn,$name)."'";
+            $sqlCount .= " AND `vacancy` = '". mysqli_real_escape_string($conn,$name)."'";
+            $url .= "&name=" . $name; 
+        }else {
+            $name = "";
+        }
+        
+        if(isset($_GET["city"]) && !empty($_GET['city'])){
+            $city = $_GET['city'];
+            $sql .= " AND `city` = '". mysqli_real_escape_string($conn,$city)."'";
+            $sqlCount .= " AND `city` = '". mysqli_real_escape_string($conn,$city)."'";
+            $url .= "&city=" . $city; 
+        }else {
+                $sql .= " AND `city` = '". mysqli_real_escape_string($conn,$city)."'";
+            $sqlCount .= " AND `city` = '". mysqli_real_escape_string($conn,$city)."'";
+            $url .= "&city=" . $city;
+        }
+        
+        if(isset($_GET["relocation"]) && !empty($_GET["relocation"])){
+            $relocation = $_GET['relocation'];
+            $sql .= " AND `relocation` = '". mysqli_real_escape_string($conn,$relocation)."'";
+            $sqlCount .= " AND `relocation` = '". mysqli_real_escape_string($conn,$relocation)."'";
+            $url .= "&relocation=" . $relocation; 
+        }else{
+            $relocation = "Невозможен";
+        }
+        
+        if(isset($_GET["costfrom"]) && !empty($_GET["costfrom"])){
+            $costfrom = $_GET['costfrom'];
+            $sql .= " AND `cost` > '". mysqli_real_escape_string($conn,(int)$costfrom)."'";
+            $sqlCount .= " AND `cost` > '". mysqli_real_escape_string($conn,(int)$costfrom)."'";
+            $url .= "&costfrom=" . $costfrom; 
+        }else{
+            $costfrom = "";
+        }
+        
+        if(isset($_GET["costto"]) && !empty($_GET["costto"])){
+            $costto = $_GET['costto'];
+            $sql .= " AND `cost` < '". mysqli_real_escape_string($conn,(int)$costto)."'";
+            $sqlCount .= " AND `cost` < '". mysqli_real_escape_string($conn,(int)$costto)."'";
+            $url .= "&costfrom=" . $costfrom; 
+        }else{
+            $costto = "";
+        }
+        
+        if(isset($_GET['employment']) && !empty($_GET['employment'])){
+            $employment = $_GET['employment'];
+            $sql .= " AND `".mysqli_real_escape_string($conn,$employment)."` = 1 ";
+            $sqlCount .= " AND `".mysqli_real_escape_string($conn,$employment)."` = 1 ";
+            $url .= "&empoyment=" . $employment; 
+        }else{
+            $employment = "employment_full";
+        }
+
+        if(isset($_GET['schedule']) && !empty($_GET['schedule'])){
+            $schedule = $_GET['schedule'];
+            $sql .= " AND `".mysqli_real_escape_string($conn,$schedule)."` = 1 ";
+            $sqlCount .= " AND `".mysqli_real_escape_string($conn,$schedule)."` = 1 ";
+            $url .= "&schedule=" . $schedule; 
+        }else{
+            $schedule = "schedule_full";
+        }
+        
+        if(isset($_GET["spec"]) && !empty($_GET['spec'])){
+            $spec = $_GET["spec"];
+            $sql  .= " AND `spec` = '". mysqli_real_escape_string($conn,$spec)."'";
+            $sqlCount .= " AND `spec` = '". mysqli_real_escape_string($conn,$spec)."'";
+            $url .= "&spec=" . $spec; 
+        }else {
+            $spec = "";
+        }
+        
+        $sql .=  " ORDER BY `update_time`";
+        
+        if($page == 1){
+            $limit = " limit 0,".$perPage;
+        }else{
+            $limit = " limit " . ($page * $perPage). ",". $perPage;
+        }
+        
+        $sql .= $limit ;
+        
+        $connection = Yii::$app->getDb();
+        
+        $command = $connection->createCommand($sql);
+        $result = $command->queryAll();
+        
+        $command = $connection->createCommand($sqlCount);
+        $count = $command->queryAll();
+        $count = (int)$count[0]["COUNT(*)"];
+        
+        $pages = $count/$perPage;
+        
+        return $this->render("resume",[
+            "name" => $name,
+            "city" => $city,
+            'spec' => $spec,
+            'costfrom' => $costfrom,
+            "costto" => $costto,
+            "employment" => $employment,
+            "schedule" => $schedule,
+            "relocation" => $relocation,
+            "result" => $result,
+            'page' => $page,
+            'pages' => $pages,
+            "url" => $url
         ]);
     }
 
