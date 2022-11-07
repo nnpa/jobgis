514933d00eb3b0ab21a3b3d8ca3d0043a0a149da
Ваше Имя <you@example.com>
2022-11-07 10:53:35 +0500

    first commit

--- models/UsersSearch.php
+++ models/UsersSearch.php
@@ -42,7 +42,7 @@ class UsersSearch extends Users
     {
         
         
-        $query = Users::find();
+        $query = Users::find()->orderBy(["id" => SORT_DESC]);
         
         // add conditions that should always apply here
 
