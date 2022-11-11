bd74b216dc0a50fcd41dd9726b5e8c5147d151fa
Ваше Имя <you@example.com>
2022-11-11 12:34:26 +0500

    first commit

--- views/vacancy/all.php
+++ views/vacancy/all.php
@@ -1,16 +1,29 @@
+<style>
+    td {
+        border:1px solid black;
+        padding:5px;
+    }
+</style>
 <h3>Мои вакансии</h3>
 <a href="/vacancy/add" class="btn btn-success">Разместить вакансию</a>
 <table>
 
     <?php foreach($vacancies as $vacancy):?>
         <tr>
-            <td style="width:30%">
+            <td>
                 <a href="/vacancy/edit?id=<?php echo $vacancy->id;?>"><?php echo $vacancy->name;?> <?php echo $vacancy->city;?> <?php echo $vacancy->costfrom;?></a>
             </td>
-            <td style="width:30%">
+            
+            <td>
+                <?php echo $vacancy->city;?> 
+            </td>
+            <td>
+                 <?php echo $vacancy->costfrom;?>
+            </td>
+            <td>
                  Добавлена <?php echo date("d.m.Y",$vacancy->create_time);?>
             </td>
-            <td style="width:30%">
+            <td>
                 <a href="/vacancy/delete?id=<?php echo $vacancy->id;?>">Удалить</a>
             </td>
         </tr>
