901eae177c2120519da3c02b5cad345941b2f2e5
Ваше Имя <you@example.com>
2022-11-11 12:15:16 +0500

    first commit

--- views/site/workers.php
+++ views/site/workers.php
@@ -4,13 +4,14 @@
         <td><b>Имя</b></td>
         <td><b>Email</b></td>
     </tr>
-</table>
 
 <?php foreach($workers as $worker):?>
    
     <tr>
-        <td><b><?php echo $worker->name . " " . $worker->surname;?></b></td>
-        <td><b><?php echo $worker->email;?></b></td>
+        <td><?php echo $worker->name . " " . $worker->surname;?></b></td>
+        <td><?php echo $worker->email;?></td>
     </tr>
+    
 <?php endforeach; ?>
+    
    <a href="/site/addworker" class="btn btn-success">Пригласить сотрудника</a>
