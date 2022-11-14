89488a0c1458cb554715fa9c369e70bad2b9b2fd
Ваше Имя <you@example.com>
2022-11-14 17:41:37 +0500

    first commit

--- modules/recruiter/views/users/addinfo.php
+++ modules/recruiter/views/users/addinfo.php
@@ -18,7 +18,7 @@ function selectCountry(val) {
     </div>
 <?php endforeach;?>
 <h3>Заполните данные</h3>
-<form method="POST">
+<form action="/recruiter/users/info" method="POST">
     Имя* <input type="text" name="name" value="<?php echo $user->name;?>"><br>
     Фамилия* <input type="text" name="surname" value="<?php echo $user->surname;?>"><br>
     Отчество* <input type="text" name="patronymic" value="<?php echo $user->patronymic;?>"><br>
