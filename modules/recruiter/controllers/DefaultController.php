dbc2b640218ed984c2844633d5de68e2a3c2ecae
Ваше Имя <you@example.com>
2022-11-14 16:09:57 +0500

    first commit

--- modules/recruiter/controllers/DefaultController.php
+++ modules/recruiter/controllers/DefaultController.php
@@ -4,6 +4,7 @@ namespace app\modules\recruiter\controllers;
 
 use yii\web\Controller;
 use Yii;
+use app\models\Users;
 
 /**
  * Default controller for the `recruiter` module
@@ -24,4 +25,28 @@ class DefaultController extends Controller
 
         return $this->goHome();
     }   
+    
+        public function actionAddinfo(){
+        $user = Yii::$app->user->identity;
+        $user  = Users::find()->where(["id" => $user->id])->one();
+        $errors = [];
+        
+        if(isset($_POST) && !empty($_POST)){
+            if(strlen($_POST["phone"]) < 18){
+               $errors[] = "Не верный формат нормера";
+            }
+            
+            if(empty($errors)){
+                $user->name = $_POST["name"];
+                $user->surname = $_POST["surname"];
+                $user->patronymic = $_POST["patronymic"];
+                $user->phone = $_POST["phone"];
+
+                $user->save(false);
+            }
+
+        }
+        
+        return $this->render("addinfo",["errors" => $errors,"user" => $user]);
+    }
 }
