77962a48c271400b6bce3b43b92ce09d9774c28f
Ваше Имя <you@example.com>
2022-11-16 16:40:52 +0500

    first commit

--- modules/recruiter/views/users/index.php
+++ modules/recruiter/views/users/index.php
@@ -19,9 +19,7 @@ $this->params['breadcrumbs'][] = $this->title;
 
     <h1><?= Html::encode($this->title) ?></h1>
 
-    <p>
-        <?= Html::a('Create Users', ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
+
 
     <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
 
