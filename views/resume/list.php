23117d1164de9b461fa782679825606857389503
Ваше Имя <you@example.com>
2022-11-18 16:46:52 +0500

    first commit

--- views/resume/list.php
+++ views/resume/list.php
@@ -1,7 +1,27 @@
+<style>
+    td{
+        border:1px solid black;
+        padding:5px;
+    }
+</style>
 <h3>Мои резюме</h3>
 <a href="/resume/new" onClick="return confirm('Создать резюме?')" class="btn btn-success">Новое резюме</a><br>
+
+<table>
+    <tr>
+        <td>Резюме</td>
+        <td>Ссылка</td>
+    </tr>
 <?php foreach($resumes as $resume):?>
-    <a href="/resume/edit?id=<?php echo $resume->id;?>"><?php echo $resume->vacancy;?></a>
-    <small><a target="_blank" href="/resume/show?id=<?php echo $resume->id;?>">Ссылка</a></small>
+    <tr>
+        <td>    
+            <a href="/resume/edit?id=<?php echo $resume->id;?>"><?php echo $resume->vacancy;?></a>
+        </td>
+        <td>   
+            <a target="_blank" href="/resume/show?id=<?php echo $resume->id;?>">Ссылка</a>
+        </td>
+    </tr>
+
     <br>
 <?php endforeach; ?>
+<table>
