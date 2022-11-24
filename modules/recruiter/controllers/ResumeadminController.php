e352e3ae87a3654a90d7b385ca0fc4cd4aabf2b0
Ваше Имя <you@example.com>
2022-11-24 11:26:24 +0500

    first commit

--- modules/recruiter/controllers/ResumeadminController.php
+++ modules/recruiter/controllers/ResumeadminController.php
@@ -78,7 +78,7 @@ class ResumeadminController extends Controller
         $url = "/admin/resumeadmin/index?test=1";
         $sqlCount = "SELECT COUNT(*) FROM `resume` WHERE 1=1 != 'Заполните должность' AND `verify` = 1";
         
-        if(!is_null($rectruiters)){
+        if(count($names)>1){
            $sql .= " AND specsub IN (" .$names. ") AND city IN (".$cities.")";
            $sqlCount .= " AND specsub IN (" .$names. ") AND city IN (".$cities.")";
         }
